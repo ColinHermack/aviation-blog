2e747acead755a3450b2b3ed681fdfaf85870702
Colin Hermack <chermack@purdue.edu>
2025-01-06 20:36:18 -0500

    Implemented search functionality

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -5,6 +5,12 @@ import { redirect } from "next/navigation";
 import React from "react";
 import { useEffect, useState } from "react";
 import { Spinner } from "@nextui-org/spinner";
+import { Card, CardBody, CardFooter } from "@nextui-org/card";
+import { Link } from "@nextui-org/link";
+import { Divider } from "@nextui-org/divider";
+import { Image } from "@nextui-org/image";
+
+import { ArticleMetadata } from "@/articles/utils";
 
 interface SearchResultsProps {
     query: string
@@ -30,6 +36,42 @@ function SearchResults(props: SearchResultsProps) {
     if (searchResults.length === 0) {
         return (
             <Spinner color="primary" size="lg" />
+        )
+    } else {
+        return (
+            searchResults.map((article: ArticleMetadata) => {
+                return (
+                    <Link
+                        key={article.title.toLowerCase().replace(" ", "-")}
+                        className="m-4"
+                        href={`/${article.type}/${article.title.toLowerCase().replaceAll(" ", "-")}`}
+                        >
+                        <Card
+                            className="w-[350px] h-[175px] max-w-full flex flex-col justify-top align-center"
+                            shadow={"md"}
+                        >
+                            <CardBody className="flex flex-row items-top justify-between overflow-visible">
+                            <h4 className="font-bold text-md w-[200px] text-left">
+                                {article.title}
+                            </h4>
+                            <Image
+                                alt="Card background"
+                                className="rounded-xl object-cover"
+                                height={100}
+                                src={`/${article.type}_images/${article.cover}`}
+                                width={100}
+                            />
+                            </CardBody>
+                            <Divider />
+                            <CardFooter>
+                            <p className="text-tiny text-default-500 font-bold">
+                                {new Date(article.datePosted).toDateString()}
+                            </p>
+                            </CardFooter>
+                        </Card>
+                    </Link>
+                )
+            })
         )
     }
 }
